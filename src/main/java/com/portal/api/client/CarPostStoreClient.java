425b37e34cd148b0694b72504ea7402d4e916a3b
Marvin Mendes <marvin.m.lopes@gmail.com>
2025-06-30 19:51:25 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/portal/api/client/CarPostStoreClient.java
@@ -0,0 +1,43 @@
+package com.portal.api.client;
+
+import com.portal.api.dto.CarPostDto;
+import com.portal.api.dto.OwnerPostDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class CarPostStoreClient {
+
+    private final String USER_STORE_SERVICE_URI = "http://localhost:8080/user";
+    private final String POST_STORE_SERVICE_URI = "http://localhost:8080/sales";
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+    public List<CarPostDto> carForSaleClient() {
+        ResponseEntity<CarPostDto[]> response = restTemplate.getForEntity(POST_STORE_SERVICE_URI + "/cars", CarPostDto[].class);
+
+        return Arrays.asList(Objects.requireNonNull(response.getBody()));
+    }
+
+    public void ownerPostsClient(OwnerPostDto newUser) {
+        restTemplate.postForEntity(USER_STORE_SERVICE_URI, newUser, OwnerPostDto.class);
+    }
+
+    public void changeCarForSaleClient(CarPostDto dto, String id) {
+        restTemplate.put(POST_STORE_SERVICE_URI+"/car/"+id, dto, CarPostDto.class);
+    }
+
+    public void deleteCarForSaleClient(String id) {
+        restTemplate.delete(POST_STORE_SERVICE_URI+"/car/"+id);
+    }
+
+
+}
---
+++ src/main/java/com/portal/api/dto/OwnerPostDto.java
@@ -0,0 +1,23 @@
+package com.portal.api.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@JsonInclude
+@NoArgsConstructor
+@AllArgsConstructor
+public class OwnerPostDto {
+
+
+    private String name;
+    private String type;
+    private String contactNumber;
+
+
+}
